41ca63f761b979c0d4db27679c52eeedb18f7a2f
Felipe Garcia <felipeedoardo@gmail.com>
2015-04-16 13:13:37 -0300

    Replaced welcome page

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+@extends('roomformilk')
+
+@section('title', "Room For Milk :: Contact")
+
+@section('h2', '<h2>ABOUT</h2>')
+
+@section('content')
+
+<div class="row">
+
+	<div class="eight columns offset-by-two">
+
+
+<h4>Your most valuable resource is your time.</h4>
+
+<p>I work helping small and online-based businesses all around the world make the most of their time.</p>
+
+<p>I'm currently working on my custom operational strategy and coaching services, but in the meantime, feel free to check my Express service.</p>
+
+
+	</div>
+
+</div>
+
+
+@stop
