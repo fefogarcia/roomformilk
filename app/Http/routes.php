f43ca3b5100c6ca1366212ac44dd1dd7610eb97b
Felipe Garcia <felipeedoardo@gmail.com>
2015-04-15 22:15:55 -0300

    Logo function done

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,30 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+
+Route::get('logo/{width}/{height}/{type}', function($width, $height, $type) {
+
+	// Create a new empty image resource in the chosen size
+	$img = Image::canvas($width, $height);
+
+	// Loads appropriate logo file and resizes it
+	$logo = Image::make('images/logo-' . $type . '.png');
+	$logo->resize($width, null, function ($constraint) {
+    	$constraint->aspectRatio();
+	});
+
+	// Pastes logo on canvas
+	$img->insert($logo, 'center');
+
+	// Displays image
+    return $img->response('png');
+
+});
+
+
+Route::get('express', function() {
+
+	return 'success';
+
+});
--- resources/views/roomformilk.blade.php
+++ resources/views/roomformilk.blade.php
@@ -26,19 +26,53 @@
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <link rel="icon" type="image/png" href="images/favicon.png">
 
+  <style>
+
+  .full-width {
+	width: 100%;
+	max-width: 100%;
+	clear: both;
+	margin: 0 auto;
+  }
+
+  </style>
+
 </head>
 <body>
 
   <!-- Primary Page Layout
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<div class="full-width" style="background-color: #3A485E; padding-top: 5%; padding-bottom: 5%">
   <div class="container">
     <div class="row">
-      <div class="one-half column" style="margin-top: 25%">
+      <div class="twelve column">
+      	<div style="text-align: center">
+        	<img src="/logo/1000/200/light" style="width: 65%" />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="full-width">
+  <div class="container">
+    <div class="row" style="margin-top: 10%">
+      <div class="one-half column">
         <h4>Basic Page</h4>
+        <p>This index.html page is a placeholder with the CSS, font and favicon. It's just waiting for you to add some content! If you need some help hit up the <a href="http://www.getskeleton.com">Skeleton documentation</a>.</p>
+        <p>This index.html page is a placeholder with the CSS, font and favicon. It's just waiting for you to add some content! If you need some help hit up the <a href="http://www.getskeleton.com">Skeleton documentation</a>.</p>
+        <p>This index.html page is a placeholder with the CSS, font and favicon. It's just waiting for you to add some content! If you need some help hit up the <a href="http://www.getskeleton.com">Skeleton documentation</a>.</p>
         <p>This index.html page is a placeholder with the CSS, font and favicon. It's just waiting for you to add some content! If you need some help hit up the <a href="http://www.getskeleton.com">Skeleton documentation</a>.</p>
       </div>
     </div>
   </div>
+</div>
+
+
+
+
+
 
 <!-- End Document
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
