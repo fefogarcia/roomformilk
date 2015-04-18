8b18e5d14f107554162e77bfc9561856ab31f9b7
Felipe Garcia <felipeedoardo@gmail.com>
2015-04-18 20:31:30 -0300

    Newsletter form refined

--- resources/views/articles/article.blade.php
+++ resources/views/articles/article.blade.php
@@ -18,23 +18,31 @@
 		    <div id="mc_embed_signup_scroll">
 		    <h5>Liked this post?</h5>
 		    <p>Sign up below and receive weekly e-mails with tips on harnessing technology to improve your work productivity.</p>
-		<div class="mc-field-group">  
-		    <label for="mce-EMAIL">Email Address </label>
-		    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+
+		<div class="row">
+
+		<div class="mc-field-group five columns">
+		    <label for="mce-FNAME">First Name</label>
+		    <input type="text" value="" name="FNAME" class="required" id="mce-FNAME" style="width: 90%">
 		</div>  
 
-		<div class="mc-field-group">  
-		    <label for="mce-FNAME">First Name </label>
-		    <input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+		<div class="mc-field-group five columns">
+		    <label for="mce-EMAIL">Email Address</label>
+		    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" style="width: 90%">
 		</div>  
 
-		<div id="mce-responses" class="clear">  
+		<div id="mce-responses" class="clear two columns">
+		    <label for="labelForSubmit">&nbsp;</label>
 		        <div class="response" id="mce-error-response" style="display:none"></div>
 		        <div class="response" id="mce-success-response" style="display:none"></div>
 		    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 		    <div style="position: absolute; left: -5000px;"><input type="text" name="b_8bf6015af5ddfba77d30c7db9_7ae91179df" tabindex="-1" value=""></div>
 		    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button button-primary"></div>
 		    </div>
+
+		</div>
+
+
 		</form>  
 		</div>  
 
