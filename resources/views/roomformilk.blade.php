db96f791f55aa14bf183bbed3dc4b94faf02ba9f
Felipe Garcia <felipeedoardo@gmail.com>
2015-04-20 06:12:45 -0300

    Added default title tag

--- resources/views/roomformilk.blade.php
+++ resources/views/roomformilk.blade.php
@@ -5,7 +5,7 @@
   <!-- Basic Page Needs
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <meta charset="utf-8">
-  <title>@yield('title')</title>
+  <title>@yield('title', 'Room For Milk')</title>
 
   @yield('meta', '
 
